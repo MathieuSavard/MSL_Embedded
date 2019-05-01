5c54eab054cb30be32939b78d249b76f54d6164e
Mathieu Savard <mathieusavard@hotmail.com>
2019-05-01 12:41:52 -0400

    Add .gitignore, .cproject, .project, project.yml, and a unfinish test for the ClearDisplay function

--- MSL_LCD/Tests/test_MSLLCDClearDisplay.c
+++ MSL_LCD/Tests/test_MSLLCDClearDisplay.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include "MSLLCD.h"
+#include "MSLLCDCore.h"
 #include "mock_MSLLCDSend.h"
 
 static eMSLReturnResponse_t eClearDisplayReturnResponse;
@@ -8,42 +8,55 @@ static eMSLReturnResponse_t eSendReturnResponse;
 
 static xMSLLCDDevice_t xMSLLCDDevice;
 static xMSLLCDPort_t xMSLLCDPort;
+static xMSLLCDDevice_t xMSLLCDDevice_expected;
+static xMSLLCDPort_t xMSLLCDPort_expected;
 
 void setUp(void)
 {
-	xMSLLCDPort.ucRS = MSL_LCD_RS_INSTRUCTION;
-	xMSLLCDPort.ucRW = MSL_LCD_RW_WRITE;
-	xMSLLCDPort.ucE = MSL_LCD_E_DISABLE;
-	xMSLLCDPort.ucDB = MSL_LCD_CLEAR_DISPLAY_MASK;
-
-	xMSLLCDDevice.ucDeviceID = 0x0000;
-	xMSLLCDDevice.ucBitMode = MSL_LCD_FUNCTION_SET_EIGHT_BIT_DATA_LENGTH;
-	xMSLLCDDevice.pxMSLLCDPort = &xMSLLCDPort;
-
 	eClearDisplayReturnResponse = eMSLError;
 	eClearDisplayReturnResponse_expected = eMSLSuccess;
 	eSendReturnResponse = eMSLSuccess;
+
+	xMSLLCDPort.ucRS = MSL_LCD_CORE_RS_INSTRUCTION;
+	xMSLLCDPort.ucRW = MSL_LCD_CORE_RW_WRITE;
+	xMSLLCDPort.ucE = MSL_LCD_CORE_E_DISABLE;
+	xMSLLCDPort.ucDB = MSL_LCD_CORE_CLEAR_DISPLAY_MASK;
+
+	xMSLLCDDevice.ucDeviceID = 0x0000;
+	xMSLLCDDevice.ucBitMode = MSL_LCD_CORE_FUNCTION_SET_EIGHT_BIT_DATA_LENGTH;
+	xMSLLCDDevice.pxMSLLCDPort = &xMSLLCDPort;
+
+	xMSLLCDPort_expected.ucRS = MSL_LCD_CORE_RS_INSTRUCTION;
+	xMSLLCDPort_expected.ucRW = MSL_LCD_CORE_RW_WRITE;
+	xMSLLCDPort_expected.ucE = MSL_LCD_CORE_E_DISABLE;
+	xMSLLCDPort_expected.ucDB = MSL_LCD_CORE_CLEAR_DISPLAY_MASK;
+
+	xMSLLCDDevice_expected.ucDeviceID = 0x0000;
+	xMSLLCDDevice_expected.ucBitMode = MSL_LCD_CORE_FUNCTION_SET_EIGHT_BIT_DATA_LENGTH;
+	xMSLLCDDevice_expected.pxMSLLCDPort = &xMSLLCDPort_expected;
+
 }
 
 void tearDown(void)
 {
 }
 
-void eMSLLCDSend_Caller()
+void vMSLLCDSend_Caller()
 {
-	eMSLLCDSend_ExpectWithArrayAndReturn( &xMSLLCDDevice, sizeof(xMSLLCDDevice), eSendReturnResponse );
+	//eMSLLCDSend_ExpectWithArrayAndReturn( &xMSLLCDDevice, sizeof(xMSLLCDDevice), eSendReturnResponse );
+	eMSLLCDSend_IgnoreAndReturn( eSendReturnResponse );
 }
 
-eMSLReturnResponse_t eMSLLCDClearDisplay_Caller()
+eMSLReturnResponse_t eMSLLCDCoreClearDisplay_Caller()
 {
-	 return eMSLLCDClearDisplay( &xMSLLCDDevice );
+	 return eMSLLCDCoreClearDisplay( &xMSLLCDDevice );
 }
 
 void test_Valid(void)
 {
-	eMSLLCDSend_Caller();
+	vMSLLCDSend_Caller();
 
-	eClearDisplayReturnResponse = eMSLLCDClearDisplay_Caller();
+	eClearDisplayReturnResponse = eMSLLCDCoreClearDisplay_Caller();
 
 	TEST_ASSERT_EQUAL( eClearDisplayReturnResponse_expected, eClearDisplayReturnResponse );
 }
